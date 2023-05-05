1361c7cdf67dfda1060518c2a7662445d7e5dd25
marcgarpstl <115546127+marcgarpstl@users.noreply.github.com>
2023-05-05 12:25:16 +0200

    Revert "Merge pull request #7 from marcgarpstl/marcbranch"
    This reverts commit ff08bc55d714f61ad140f20c25e7aa277434dcb5, reversing
    changes made to 24d3992b61231106706775ed1a35c82e5c458b31.

---
+++ WebAppAPI/Pages/Update.cshtml.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using WebAppAPI.DatabaseContext;
+using WebAppAPI.Models;
+
+namespace WebAppAPI.Pages
+{
+    public class UpdateModel : PageModel
+    {
+        DataBaseContext dbc;
+
+        public UpdateModel(DataBaseContext dbc)
+        {
+            this.dbc = dbc;
+        }
+        [BindProperty]
+        public Service service { get; set; }
+
+        public void OnGet(int Id)
+        {
+            service = dbc.Services.Find(Id);
+        }
+        public void OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                Service serviceReg = dbc.Services.Find(service.Id);
+                
+                serviceReg.Name = service.Name;
+                serviceReg.Price = service.Price;
+                serviceReg.Description = service.Description;
+                serviceReg.IsAvalible = service.IsAvalible;
+                
+                dbc.SaveChanges();
+                
+            }
+        }
+    }
+}
