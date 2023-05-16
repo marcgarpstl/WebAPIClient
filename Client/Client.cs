9cadb74da483526bedc5ea56923c2147a9c8aa14
marcgarpstl <115546127+marcgarpstl@users.noreply.github.com>
2023-05-16 14:58:35 +0200

    New controller

---
+++ Client/Client.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Client
+    {
+        HttpClient http = new();
+        string url = "https://localhost:7238/api/Services";
+
+        public List<Service> GetServices()
+        {
+            Uri uri = new Uri(url);
+
+            HttpResponseMessage response = http.GetAsync(uri).Result;
+            if(response.IsSuccessStatusCode)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                return JsonConvert.DeserializeObject<List<Service>>(json);
+            }
+
+            Console.WriteLine("Error. " + (int)response.StatusCode + " : " + response.StatusCode);
+            return new List<Service>();
+        }
+        public bool AddService(string name, int price,  string description)
+        {
+            Service newService = new() {Name = name, Price = price, Description = description, IsAvalible = true };
+            
+            Uri uri = new Uri(url);
+
+            string jayson = JsonConvert.SerializeObject(newService);
+            Console.WriteLine("sending json: " + jayson);
+
+            StringContent stringContent = new StringContent(jayson, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = http.PostAsync(uri, stringContent).Result;
+
+            if(response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(name + " was added to the database.");
+                return true;
+            }
+
+            Console.WriteLine("Error. " + (int)response.StatusCode + " : " + response.StatusCode);
+            return false;
+        }
+
+        public bool RemoveService(int id) 
+        {
+            Uri uri = new Uri(url + "?id=" + id);
+
+            HttpResponseMessage response = http.DeleteAsync(uri).Result;
+
+            if(response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Service with id: " + id + " has been removed");
+                return true;
+            }
+
+            Console.WriteLine("Error. " + (int)response.StatusCode + " : " + response.StatusCode);
+            return false;
+        }
+    }
+}
---
+++ Client/ServiceHub.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class ServiceHub
+    {
+        List<Service> services;
+
+        Client client = new();
+
+        public ServiceHub()
+        {
+            ShowAllServices();
+        }
+
+        public void ShowAllServices()
+        {
+            services = client.GetServices();
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("Create - Create a new service");
+                Console.WriteLine("Read - Show all services");
+                Console.WriteLine("Update - Update existing service");
+                Console.WriteLine("Delete - Delete service");
+                string input = Console.ReadLine().ToLower();
+                switch (input)
+                {
+                    case "create":
+                        NewService();
+                        break;
+                    case "read":
+                        LoadAllServices();
+                        break;
+                    case "update":
+                        //Update();
+                        break;
+                    case "delete":
+                        DeleteService();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Please write from options above");
+                        Console.WriteLine("Press any key to try again.");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+
+        private void DeleteService()
+        {
+            Console.WriteLine("Please provide the Id of the service you want to remove");
+            int id;
+            try
+            {
+                id = int.Parse(Console.ReadLine());
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Only Id is accepted");
+                return;
+            }
+            bool success = client.RemoveService(id);
+            if(success)
+            {
+                services.Remove(services.SingleOrDefault(services => services.Id == id));
+            }
+        }
+
+        private void NewService()
+        {
+
+            Console.Write("Please provide a name for the service: ");
+            string name = Console.ReadLine();
+            Console.Write("Please provide a price for the service: ");
+            int price = int.Parse(Console.ReadLine());
+            Console.Write("Describe the service you would like to create: ");
+            string description = Console.ReadLine();
+
+            bool success = client.AddService(name, price, description); 
+            if(success)
+            {
+                ShowAllServices();
+            }
+
+        }
+
+        private void LoadAllServices()
+        {
+            foreach (Service service in services)
+            {
+                service.LoadService();
+            }
+        }
+    }
+}
